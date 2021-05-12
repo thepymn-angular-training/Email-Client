d1a4d66d515a6f83726d2cd6f0c0db205f247424
Peyman <usestate.pk@gmail.com>
2021-05-12 18:59:30 +0430

    form validation completed

--- src/app/shared/input/input.component.ts
+++ src/app/shared/input/input.component.ts
@@ -9,10 +9,17 @@ import { FormControl } from '@angular/forms';
 export class InputComponent implements OnInit {
   @Input() label: string;
   @Input() control: FormControl;
+  @Input() inputType: string;
 
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  showErrors() {
+    const { dirty, touched, errors } = this.control;
+
+    return dirty && touched && errors;
   }
 
 }
