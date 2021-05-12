6df38355535e5b6d296a8c20f5ed356ef26e1652
Peyman <usestate.pk@gmail.com>
2021-05-12 23:05:59 +0430

    signup request added

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
+interface SignupCredentials {
+  username: string;
+  password: string;
+  passwordConfirmation: string;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class AuthService {
+  rootUrl = 'https://api.angular-email.com';
+
   constructor(private http: HttpClient) {}
 
   usernameAvailable(username: string) {
-    return this.http.post<{ available: boolean }>('https://api.angular-email.com/auth/username', {
-      username,
-    });
+    return this.http.post<{ available: boolean }>(
+      `${this.rootUrl}/auth/username`,
+      {
+        username,
+      }
+    );
+  }
+
+  signup(credentials: SignupCredentials) {
+    return this.http.post<{ username: string }>(
+      `${this.rootUrl}/auth/signup`,
+      credentials
+    );
   }
 }
