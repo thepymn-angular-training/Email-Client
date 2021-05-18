6a9fdca077d0a9f093f72118ffa18c1dd285bcb8
Peyman <usestate.pk@gmail.com>
2021-05-18 17:04:21 +0430

    added email index

--- src/app/inbox/inbox.module.ts
+++ src/app/inbox/inbox.module.ts
@@ -3,15 +3,25 @@ import { CommonModule } from '@angular/common';
 
 import { InboxRoutingModule } from './inbox-routing.module';
 import { HomeComponent } from './home/home.component';
+import { EmailCreateComponent } from './email-create/email-create.component';
+import { EmailShowComponent } from './email-show/email-show.component';
+import { EmailIndexComponent } from './email-index/email-index.component';
+import { EmailReplyComponent } from './email-reply/email-reply.component';
+import { HttpClientModule } from '@angular/common/http';
 
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    EmailCreateComponent,
+    EmailShowComponent,
+    EmailIndexComponent,
+    EmailReplyComponent
   ],
   imports: [
     CommonModule,
-    InboxRoutingModule
+    InboxRoutingModule,
+    HttpClientModule
   ]
 })
 export class InboxModule { }
